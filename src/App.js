2c358f3b2bc8116ac083187681b47c7b8db9b604
platisam <platisam@gmail.com>
2023-09-07 15:31:39 +0200

    adding Review component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import React from "react";
+import Review from "./Review";
+
 function App() {
-  return <div className="App">Reviews Project</div>;
+  return (
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>our reviews</h2>
+          <div className="underline"></div>
+        </div>
+        <Review />
+      </section>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/Review.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Review = () => {
+  return <h2>review component</h2>;
+};
+
+export default Review;
