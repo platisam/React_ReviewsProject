cc9e65b2058bc7799dda95f0f44e270b9bb68ff4
platisam <platisam@gmail.com>
2023-09-07 15:53:24 +0200

    destructuring props of people array and displaying elements of Review component

--- src/Review.js
+++ src/Review.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import people from "./data";
+import { FaQuoteRight, FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+  // console.log(people);
+
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+    </article>
+  );
 };
 
 export default Review;
